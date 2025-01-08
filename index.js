3167a5676bc93620453d23141ca49e3675af407d
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-01-08 13:20:54 +0100

    Added red and green color to the input outline depending on the result

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 submitButton.addEventListener('click', function (event) {
   event.preventDefault();
   
-  emailValue = emailInput.value.trim();
+  const emailValue = emailInput.value.trim();
 
   if (emailValue === '') {
     emailResponse.textContent = 'Whoops! It looks like you forgot to add your email';
+    emailInput.style.outline = '0.1rem solid hsl(354, 100%, 66%)';
   } else if (!emailPattern.test(emailValue)) {
     emailResponse.textContent = 'Please provide a valid email address';
+    emailInput.style.outline = '0.1rem solid hsl(354, 100%, 66%)';
   } else {
     emailResponse.textContent = '';
+    emailInput.style.outline = '0.1rem solid hsl(120, 100%, 40%)';
+    emailInput.value = '';
   }
 });
 
