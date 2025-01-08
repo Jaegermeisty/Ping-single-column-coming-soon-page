ce180b1cf94b2cf17b29de7c883b82ebb476e803
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-01-08 12:33:26 +0100

    Add button click event listener and connect script to HTML

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const emailInput = document.getElementById('email-input');
 const submitButton = document.getElementById('submit-btn');
 const emailResponse = document.querySelector('.email-response');
+
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  console.log('Button clicked');
+});
